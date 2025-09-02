28087f6bc4c5a76cee4a8757d1d0a377533b5c54
Olexiy-repin <repinlyoha@gmail.com>
2025-09-02 12:28:49 +0300

    solution

---
+++ js/04-tasks-solutions.js
@@ -0,0 +1,31 @@
+/*
+TODO: Напиши функцію checkCaseOfLetter(letter), яка буде перевіряти регістр літери.
+TODO: В параметр letter будуть передавати рядок із однією літерою.
+TODO: Якщо літера знаходиться у верхньому регістрі, то буде повертатися 'upper case' інакше 'lower case'.
+*/
+
+// const checkCaseOfLetter = function (letter) {
+//   return letter === letter.toUpperCase() ? 'upper case' : 'lower case';
+// };
+
+// console.log(checkCaseOfLetter('a')); // 'lower case'
+// console.log(checkCaseOfLetter('A')); // 'upper case'
+
+/*
+TODO: Напиши функцію changeCaseOfLetters(str), яка буде повертати рядок із зміненим регістром літер.
+TODO: В параметр str будуть передавати рядок.
+*/
+
+// const changeCaseOfLetters = function (str) {
+//   let result = '';
+
+//   for (const char of str) {
+//     result += char === char.toUpperCase() ? char.toLowerCase() : char.toUpperCase();
+//   }
+
+//   return result;
+// };
+
+// console.log(changeCaseOfLetters('AbCdE')); // aBcDe
+// console.log(changeCaseOfLetters('abc')); // ABC
+// console.log(changeCaseOfLetters('CBA')); // cba
